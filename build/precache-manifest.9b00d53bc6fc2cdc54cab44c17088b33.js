0b66430398f15311517f4234a3d84d8d428a25cf
unknown <Muhammad Hassan@DESKTOP-F7VKQPU.itempire.server>
2019-03-26 14:19:04 +0500

    Remover Build folder from gitignore

---
+++ build/precache-manifest.9b00d53bc6fc2cdc54cab44c17088b33.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "79c75635a6eb60d85da6",
+    "url": "/static/js/main.79c75635.chunk.js"
+  },
+  {
+    "revision": "61fd8929a94079590ef1",
+    "url": "/static/js/1.61fd8929.chunk.js"
+  },
+  {
+    "revision": "2c560a74383a6676a3db7fc3560e5886",
+    "url": "/index.html"
+  }
+];
